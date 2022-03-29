a3c3d18f52eb711514ae791764da29d35c521ba8
Rukavanov Viacheslav <v.rukavanov@voskhod.ru>
2022-03-29 17:49:35 +0300

    создал questions

---
+++ src/main/java/ru/sli/stack/repository/Questions.java
@@ -0,0 +1,12 @@
+package ru.sli.stack.repository;
+
+public class Questions {
+
+    public String title;
+    public String description;
+
+    public Questions(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+}
