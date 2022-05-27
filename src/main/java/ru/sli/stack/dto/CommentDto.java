184c1b21d2171fb97c3601702e9cc5f42e97b930
slava <rukavanov_v@mail.ru>
2022-05-27 14:39:14 +0300

    возврат комментария через commentDto

--- src/main/java/ru/sli/stack/dto/CommentDto.java
+++ src/main/java/ru/sli/stack/dto/CommentDto.java
@@ -2,13 +2,27 @@ package ru.sli.stack.dto;
 
 import io.swagger.annotations.ApiModelProperty;
 
+import java.time.ZonedDateTime;
+
 public class CommentDto {
     @ApiModelProperty(value = "комментарий", example = "коммент 1")
     private String comment;
-    private int id;
+    private Integer id;
     private int questionId;
 
-    public CommentDto(String comment, int id, int questionId) {
+    private ZonedDateTime createdAt;
+
+    public CommentDto() {
+    }
+
+    public CommentDto(String comment, Integer id, int questionId, ZonedDateTime createdAt) {
+        this.comment = comment;
+        this.id = id;
+        this.questionId = questionId;
+        this.createdAt = createdAt;
+    }
+
+    public CommentDto(String comment, Integer id, int questionId) {
         this.comment = comment;
         this.id = id;
         this.questionId = questionId;
@@ -22,12 +36,20 @@ public class CommentDto {
         this.comment = comment;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(ZonedDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 
     public int getQuestionId() {
