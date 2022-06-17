5d1e80bcdf5573714e0020b2f357526b4071776f
slava <rukavanov_v@mail.ru>
2022-06-17 10:13:47 +0300

    SpringSecurity роли в ArrayList

---
+++ src/main/java/ru/sli/stack/repository/User.java
@@ -0,0 +1,58 @@
+package ru.sli.stack.repository;
+
+
+import javax.persistence.*;
+
+@Entity(name = "User")
+@Table(name = "users", schema = "stack")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+//    @ManyToMany
+//    private Collection<Role> roles;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+//    public Collection<Role> getRoles() {
+//        return roles;
+//    }
+//
+//    public void setRoles(Collection<Role> roles) {
+//        this.roles = roles;
+//    }
+}
